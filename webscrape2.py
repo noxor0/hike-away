82c8cc74e82715c1179456049b093186e9cd07e7
Connor <avaterq0@gmail.com>
2017-03-25 16:22:59 -0700

    json file added

--- webscrape2.py
+++ webscrape2.py
@@ -1,4 +1,4 @@
-# import requests, json
+import requests, json
 # r = requests.get("http://www.wta.org/go-outside/map/@@trailhead-search/getHikes?jsonp_callback=WtaTrailheadSearch.setHikes")
 # pre_text = r.text
 # text = pre_text[28:len(pre_text)-1]
@@ -11,9 +11,7 @@
 # print json.dumps(hike_list)
 import ast
 
-# with open("hikes.json", "r") as file_in:
-#     for line in file_in:
-#         new_dict = ast.literal_eval(line[1:len(line)-1])
-#         print new_dict
-
-new_dict = ast.literal_eval("{"rating": "0.0", "length": "14.0", "kml": "", "features": "rmvpe", "name": "Grand Park via Sunrise", "lat": "46.9143", "lng": "-121.6402", "elevGain": null, "id": "}")
+with open("hikes.txt", "rb") as file_in:
+    content = json.load(file_in)
+with open("stringJson.txt", "wb") as fout:
+    json.dump(content, fout, indent=1)
