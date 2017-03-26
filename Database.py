483b03561f7d33c4b4af709188fdb9b121877dc1
RhaydenX <alamb25@uw.edu>
2017-03-26 10:23:49 -0700

    Connor the god

--- Database.py
+++ Database.py
@@ -12,24 +12,29 @@ class Database(object):
 
 
         self.cursor = self.conn.cursor()
-        
-
     def add_trails(self):
         with open('hikes.json') as data_file:    
-            data = json.load(data_file, parse_float=Decimal)
+            data = json.load(data_file)
 
         for trail in data:
-            distance = float(trail['length'])
-            lat = float(trail['lat'])
-            lng = float(trail['lng'])
-            if trail['elevGain'] is None:
-                gain = 0
-            else:
-                gain = float(trail['elevGain'])
-            diff = get_difficulty(distance, gain)
-            self.cursor.execute("INSERT INTO Trail(trailID, trailName, difficulty, lat, long)\
-                                 VALUES ('%s', '%s', '%d', '%d', '%d')" % \
-                                 (trail['id'], trail['name'], diff, lat, lng))
-        self.cursor.commit()
+            try:
+                distance = float(trail['length'])
+                lat = float(trail['lat'])
+                lng = float(trail['lng'])
+                if trail['elevGain'] is None:
+                    gain = 0
+                else:
+                    gain = float(trail['elevGain'])
+                diff = get_difficulty(distance, gain)
+                sql = """INSERT INTO Trail(trailID, trailName, difficulty, lat, lng) VALUES ('%s', "%s", '%3.1f', '%7.4f', '%7.4f')""" % (trail['id'], trail['name'], diff, lat, lng)
+                print sql
+                try:
+                    self.cursor.execute(sql)
+                    self.conn.commit()
+                except:
+                    self.conn.rollback()
+            except:
+                pass
+        self.conn.close()
 test = Database()
 test.add_trails()
