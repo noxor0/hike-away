7a6d6a22a0db7a3b1de9c4c8ff69320c7da4544b
RhaydenX <alamb25@uw.edu>
2017-03-26 10:41:57 -0700

    Hike class per Connor's request

---
+++ Hike.py
@@ -0,0 +1,8 @@
+
+class Hike(object):
+    def __init__(self, trail_id, name, difficulty, lat, lng):
+        self.trail_id = trail_id
+        self.name = name
+        self.difficulty = difficulty
+        self.lat = lat
+        self.lng = lng
